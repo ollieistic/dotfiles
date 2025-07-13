d928e0f7cd0a098bd71f6a54b6687837c337b13d
ollieistic <135356998+ollieistic@users.noreply.github.com>
2025-07-13 13:26:47 +0200

    Auto backup: 2025-07-13 13:26:47

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 #### Ollieistic's Dotfiles Setup Script ####
 ############################################
 
-# Symlink .bashrc
+# Symlink for .bashrc
 ln -sf "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
 
-# Symlink Hyprland config
-ln -sf "$HOME/dotfiles/.config/hyprland/hyprland.conf" "$HOME/.config/hyprland/hyprland.conf"
+# Symlink for all Hypr configs
+ln -sf "$HOME/dotfiles/hypr" "$HOME/.config/hypr"
 
-# Symlink kitty config
-ln -sf "$HOME/dotfiles/.config/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
+# Symlink for Kitty config
+ln -sf "$HOME/dotfiles/kitty" "$HOME/.config/kitty"
+
+# Symlink for wallpapers
+ln -sf "$HOME/dotfiles/wallpapers" "$HOME/Pictures/wallpapers"
