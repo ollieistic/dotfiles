a8446b44d60bdf85ae9d0ef7633c5a09e0e93b83
ollieistic <135356998+ollieistic@users.noreply.github.com>
2025-07-14 22:33:58 +0200

    Auto backup: 2025-07-14 22:33:58

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ cd "$HOME/dotfiles" || exit 1
 git add -A
 
 if ! git diff --cached --quiet; then
-    git commit -m "Automatic backup: $(date '+%Y-%m-%d %H:%M:%S')"
+    git commit -m "Auto backup: $(date '+%Y-%m-%d %H:%M:%S')"
     
     git pull origin main --rebase
     git push origin main
